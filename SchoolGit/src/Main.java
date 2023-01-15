faab280e3b1d19fb7075f68389f68ef6ec8c2e84
Semen_Udin <sem.iudin2014@yandex.ru>
2023-01-15 22:31:20 +0300

    Homework 16.01.2023

--- SchoolGit/src/Main.java
+++ SchoolGit/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double pFirst = in.nextDouble();
+        double pSecond = in.nextDouble();
+        SchoolTasks.Ski(pFirst);
+        SchoolTasks.Bank(pSecond);
+        System.out.println();
     }
 }
---
+++ SchoolGit/src/SchoolTasks.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SchoolTasks {
+    public static void Ski(Double p){
+        double sumLen = 10, lenAll = 10;
+        int count = 1;
+        while (sumLen < 200){
+            lenAll = lenAll + (lenAll * (p / 100));
+            sumLen += lenAll;
+            count++;
+        }
+        System.out.println("K = " + count);
+        String tempSumLen = String.format("%.2f", sumLen);
+        System.out.println("S = " + tempSumLen);
+    }
+
+    public static void Bank(Double p){
+        int count = 0;
+        double sum = 1000;
+        while (sum < 1100){
+            sum = sum + (sum * (p / 100));
+            count++;
+        }
+        System.out.println();
+        System.out.println("K = " + count);
+        String tempSum = String.format("%.2f", sum);
+        System.out.println("S = " + tempSum);
+    }
+
+}
